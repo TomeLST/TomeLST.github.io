90e5e1ddb9a5380bef8388e8379756e4ab7674d7
TomeLST <3039731980@qq.com>
2022-01-23 18:30:16 +0800

    Site updated: 2022-01-23 18:30:16

---
+++ dist/music.js
@@ -0,0 +1,31 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+      {
+        name: "花海",
+        artist: 'JayChou',
+        url: 'http://www.shiyinren.com/kw/antiserver.kuwo.cn/anti.s?rid=MUSIC_440615&response=res&format=mp3|aac&type=convert_url&br=128kmp3&agent=iPhone&callback=getlink&jpcallback=getlink.mp3',
+        cover: 'http://ww1.sinaimg.cn/large/6a011e49ly1g4ae7xvaiwj20tc0xc0y2.jpg',
+      },
+      {
+        name: '东风破',
+        artist: 'JayChou',
+        url: 'http://www.shiyinren.com/kw/antiserver.kuwo.cn/anti.s?rid=MUSIC_79479&response=res&format=mp3|aac&type=convert_url&br=128kmp3&agent=iPhone&callback=getlink&jpcallback=getlink.mp3',
+        cover: 'http://ww1.sinaimg.cn/large/6a011e49ly1g4ae7xoj0bj20oy0xcdkp.jpg',
+      },
+      {
+        name: '爱在西元前',
+        artist: 'JayChou',
+        url: 'http://www.shiyinren.com/kw/antiserver.kuwo.cn/anti.s?rid=MUSIC_588552&response=res&format=mp3|aac&type=convert_url&br=128kmp3&agent=iPhone&callback=getlink&jpcallback=getlink.mp3',
+        cover: 'http://ww1.sinaimg.cn/large/6a011e49ly1g4ae7xro33j20u00wraf8.jpg',
+      },
+      {
+        name: '兰亭序',
+        artist: 'JayChou',
+        url: 'http://www.shiyinren.com/kw/antiserver.kuwo.cn/anti.s?rid=MUSIC_174338259&response=res&format=mp3|aac&type=convert_url&br=128kmp3&agent=iPhone&callback=getlink&jpcallback=getlink.mp3',
+        cover: 'http://ww1.sinaimg.cn/large/6a011e49ly1g4ae7xoj0bj20oy0xcdkp.jpg',
+      }
+    ]
+});
